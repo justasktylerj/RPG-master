026954e7a38b69572d1a5e278ce614c9c8030278
Tyler jarrard <tjar2074@740s-pl-104.local>
2016-01-07 09:27:19 -0700

    adasd

--- src/rpgView/RPGPanel.java
+++ src/rpgView/RPGPanel.java
@@ -18,6 +18,7 @@ public class RPGPanel extends JPanel
 	private JLabel health;
 	private JCheckBox mobStats;
 	private JLabel nameLabel;
+	private JLabel healthMob;
 	private JLabel descriptionLabel;
 	private int goblinNumberPanel;
 	public boolean isAttacking;
@@ -30,10 +31,12 @@ public class RPGPanel extends JPanel
 		this.baseController = baseController;
 		baseLayout = new SpringLayout();
 		attackButton = new JButton("ATTACK");
-		health = new JLabel("20/20");
+		health = new JLabel("Health: 20/20");
 		mobStats = new JCheckBox("monster's stats");
 		nameLabel = new JLabel(tempGoblins[goblinNumberPanel].getName());
 		descriptionLabel = new JLabel(tempGoblins[goblinNumberPanel].getDescription());
+		healthMob = new JLabel("" + tempGoblins[goblinNumberPanel].getMobHealthCurrent());
+		
 		goblinNumberPanel = 1;
 		isAttacking = false;
 		
@@ -53,9 +56,11 @@ public class RPGPanel extends JPanel
 		this.add(health);
 		this.add(mobStats);
 		this.add(nameLabel);
+		this.add(healthMob);
 		this.add(descriptionLabel);
 		nameLabel.setVisible(false);
 		descriptionLabel.setVisible(false);
+		healthMob.setVisible(false);
 	
 		
 	}
@@ -70,6 +75,10 @@ public class RPGPanel extends JPanel
 		baseLayout.putConstraint(SpringLayout.SOUTH, nameLabel, -6, SpringLayout.NORTH, descriptionLabel);
 		baseLayout.putConstraint(SpringLayout.SOUTH, descriptionLabel, -58, SpringLayout.SOUTH, this);
 		baseLayout.putConstraint(SpringLayout.EAST, descriptionLabel, -65, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, health, 10, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, health, 0, SpringLayout.WEST, attackButton);
+		baseLayout.putConstraint(SpringLayout.NORTH, healthMob, 6, SpringLayout.SOUTH, descriptionLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, healthMob, 0, SpringLayout.WEST, nameLabel);
 	}
 	
 	private void setupListeners()
@@ -83,9 +92,11 @@ public class RPGPanel extends JPanel
 				if(mobStats.isSelected())
 				{
 					nameLabel.setVisible(true);
+					healthMob.setVisible(true);
 					descriptionLabel.setVisible(true);
 					nameLabel.setText(tempGoblins[goblinNumberPanel].getName());
 					descriptionLabel.setText(tempGoblins[goblinNumberPanel].getDescription());
+					healthMob.setText("Health:" + tempGoblins[goblinNumberPanel].getMobHealthCurrent());
 				}
 				else
 				{
@@ -101,7 +112,7 @@ public class RPGPanel extends JPanel
 			{
 				isAttacking = true;
 				baseController.getPlayerHealthCurrent();
-				health.setText(baseController.getPlayerHealthCurrent() + "/" + baseController.getPlayerHealthMax());
+				health.setText("Health: " + baseController.getPlayerHealthCurrent() + "/" + baseController.getPlayerHealthMax());
 			}
 		});
 	}
